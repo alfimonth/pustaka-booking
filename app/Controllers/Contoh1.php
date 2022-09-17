fde61721f82fff54f023b48ede9960f2195e5da2
alfimonth <alvy1610@gmail.com>
2022-09-17 14:42:17 +0700

    Membuat Contoh1.php

--- app/Controllers/Contoh1.php
+++ app/Controllers/Contoh1.php
@@ -7,6 +7,6 @@ class Contoh1 extends BaseController
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Alfi Syahrin Saya tingga di daerah Wonogiri olah raga yang saya sukai adalah Bulutangkis";
+        echo "Nama saya Alfi Syahrin Saya tinggal di daerah Wonogiri olah raga yang saya sukai adalah Bulutangkis";
     }
 }

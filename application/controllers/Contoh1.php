e2fc01d01c4acadd8f5101aea8b6e96cf9f0b3fe
alfimonth <alvy1610@gmail.com>
2022-09-23 13:59:00 +0700

    menambah COntoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Alfi Syahrin Saya tinggal di daerah Wonogiri olah raga yang saya sukai adalah Bulutangkis";
+    }
+}

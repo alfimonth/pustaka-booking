abbed82005a44212583974efc6af3d7193177902
alfimonth <alvy1610@gmail.com>
2022-09-23 15:32:18 +0700

    h

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -5,6 +5,6 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Namaku Alfi Syahrin, saya tinggal di daerah Wonogiri olah raga yang saya sukai adalah Bulutangkis.";
+        echo "Namaku Alfi Syahrin, saya tinggal di daerahh Wonogiri olah raga yang saya sukai adalah Bulutangkis.";
     }
 }

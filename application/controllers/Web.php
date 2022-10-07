0fbffc3fd8df9979ffd33c6d33203dcfeae6d6a6
alfimonth <alvy1610@gmail.com>
2022-10-07 15:19:21 +0700

    setting base_url

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -4,11 +4,16 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Web extends CI_Controller
 {
 
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
     public function index()
     {
         $data['judul'] = 'Halaman Depan';
         $this->load->view('v_header', $data);
-        $this->load->view('v_index', $data);
-        $this->load->view('v_footer', $data);
+        $this->load->view('v_index');
+        $this->load->view('v_footer');
     }
 }

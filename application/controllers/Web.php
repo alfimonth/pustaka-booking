8b518f0399a7070ed930749f3278a45c89753237
alfimonth <alvy1610@gmail.com>
2022-10-07 15:46:46 +0700

    menambah about

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -16,4 +16,11 @@ class Web extends CI_Controller
         $this->load->view('v_index');
         $this->load->view('v_footer');
     }
+    public function about()
+    {
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_about', $data);
+        $this->load->view('v_footer', $data);
+    }
 }
--- application/views/v_header.php
+++ application/views/v_header.php
@@ -2,13 +2,13 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
+    <meta charset=utf-8>
     <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
+    <link rel=stylesheet type=text/css href=<?php echo base_url() ?>assets/css/stylebuku.css>
 </head>
 
 <body>
-    <div id="wrapper">
+    <div id=wrapper>
         <header>
             <hgroup>
                 <h1>RentalBuku.net</h1>
@@ -16,9 +16,9 @@
             </hgroup>
             <nav>
                 <ul>
-                    <li><a href="<?php echo base_url() . 'index . php / web' ?>">Home</a></li>
-                    <li><a href="<?php echo base_url() . 'index . php / web / about' ?>">About</a></li>
+                    <li><a href=<?php echo base_url() . 'web' ?>>Home</a></li>
+                    <li><a href=<?php echo base_url()  . 'web/about' ?>>About</a></li>
                 </ul>
             </nav>
-            <div class="clear"></div>
+            <div class=clear></div>
         </header>
